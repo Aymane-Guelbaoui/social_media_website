a259c139e7d4661003cd6ffa3122d7d11dac3753
AymaneGl <ay.baoui2016@gmail.com>
2025-09-26 18:25:19 +0100

    Update file.txt with latest changes

---
+++ backend/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+
+python manage.py collectstatic --noinput
+
+# python manage.py migrate --noinput
+
+exec gunicorn backend.wsgi:application --bind 0.0.0.0:${PORT:-8080} --workers 3
